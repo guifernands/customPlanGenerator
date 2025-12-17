4a61bbf1d4a2ee99d85c946eadf15208ba2fee76
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2025-12-17 17:17:34 -0300

    right month, day and year

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,16 +9,28 @@ btnGerar.addEventListener('click', function() {
         return;
     }
 
-    // por padrao a data vem "2025-04", se separarmos ela pelo '-' temos o mes e o ano separados
+    // por padrao a data vem "2025-04", se separarmos ela pelo '-' temos o mes e o ano separados, vem em String naturalmente e o .map(Number) converte
     const partes = inputData.split('-').map(Number);
 
     const ano = partes[0]; // pega o primeiro numero (2025)
     const mes = partes[1]; // pega o segundo numero (04)
 
-    // testar
-    console.log("Ano:", ano);
-    console.log("Mês do Input:", mes);
+    let dataCorrente = new Date(ano, mes - 1, 1);
 
-    // JavaScript conta meses do 0 ao 11, subtrair 1 na hora de criar a data
-    console.log("Mês para o JS (Mês - 1):", mes - 1);
+    console.clear();
+    console.log("--- Iniciando Cálculo Bimestral ---");
+
+    for(let i = 0; i < 2; i++) {
+        let anoLoop = dataCorrente.getFullYear();
+        let mesLoop = dataCorrente.getMonth();
+
+        //                          ano, mes e dia
+        let ultimoDia = new Date(anoLoop, mesLoop + 1, 0).getDate();
+
+        console.log(`Rodada ${i + 1}: Mês ${mesLoop + 1}/${anoLoop}`);
+        console.log(`Este mês tem ${ultimoDia} dias.`);
+
+        // converte o mês e já faz virada de ano sozinho
+        dataCorrente.setMonth(dataCorrente.getMonth() + 1);
+    }
 });
